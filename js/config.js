c95eacc91324f87fa985c24af8e290b326899cdb
edmggroup <krishnakumar.m@christuniversity.in>
2026-09-09 15:08:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ window.LAB_CONFIG = {
   /* Paste the /exec URL of your Apps Script web app here to turn
      on "Submit to the department sheet". Leave it empty and the
      button disappears; students then download the report instead. */
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzCWz-g2oxnQFSvtESdJ2163gOLZqfNmZu7Q_dCor0LQUafHfqlmJGK6CgmcILJPPQx/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz-Dyp8Py7Zl6xsD2m6mDR8crvpoYJsnMdqqgaip_sxzq69U7pC-8Aqf5IvHRV1gnfIhQ/exec",
 
   /* Printed on the report header. */
   DEPARTMENT: "Department of Physics and Electronics",
